e4b1d4e79af307fbecb2159588cba861cf3f96b2
Boneseye0 <svrosevelt@gmail.com>
2026-03-05 16:02:52 +0100

    work in progress
    To do, split functions off into components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,78 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from 'vue';
+//import RollDice from './components/RollDice.vue'
+
+let throwCount = ref('6')
+
+let diceRolled = ref([]);
+
+const rollDice = () =>{
+    diceRolled.value = [];    
+for (let index = 0; index < throwCount.value; index++) {
+  const randomNumber = Math.ceil(Math.random() * 6);
+                      diceRolled.value.push(randomNumber);}
+}
+
+const computedDiceCount = computed(() => { const diceCount = {
+  1 : 0,
+  2 : 0,
+  3 : 0,
+  4 : 0,
+  5 : 0,
+  6 : 0,
+};
+diceRolled.value.forEach(value => diceCount[value]++);
+return diceCount;
+})
+ /*                 
+let computedDiceCount = computed(() => {
+  for (let index = 0; index < diceRolled.value.length; index++) {
+  diceCount[diceRolled.value[index]] ++;
+  }
+})
+let diceCount = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+};
+  */
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   <input type="number" v-model="throwCount"><br>
+   <button @click="rollDice">Throw</button>
+
+   <br>
+    <p>{{ diceRolled }}</p>
+    <ul>
+      <li v-for="(value, key, index) in computedDiceCount">
+      {{ key }} : {{ value }}
+      </li>
+      
+    </ul>
+    <table>
+    <thead>
+      <tr>
+        <td>Getal</td><td>aantal</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr><td>1</td><td>{{ computedDiceCount[1] }}</td></tr>
+      <tr><td>2</td><td>{{ computedDiceCount[2] }}</td></tr>
+      <tr><td>3</td><td>{{ computedDiceCount[3] }}</td></tr>
+      <tr><td>4</td><td>{{ computedDiceCount[4] }}</td></tr>
+      <tr><td>5</td><td>{{ computedDiceCount[5] }}</td></tr>
+      <tr><td>6</td><td>{{ computedDiceCount[6] }}</td></tr>
+    </tbody>
+    </table>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/RollDice.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(
+    {
+        throwCount: Number,
+    }
+);
+const emit = defineEmits([diceRolled]);
+
+const emitevent = () =>{
+    emit 
+}
+const rollDice = () =>{
+    
+    for (let index = 0; index < throwCount; index++) {
+                const randomNumber = Math.ceil(Math.random() * 6);
+                diceRolled.push(randomNumber);
+                diceCount[randomNumber] ++;
+        
+    };
+}
+    
+    </script>
+
+    <template>
+        <button @click="emitEvent"></button>
+    </template>
